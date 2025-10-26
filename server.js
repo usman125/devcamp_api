aecc3f4529243ca9c6236d20e7ac6b056d7c1818
Usman Ali Khan <usmanalikhan00@gmail.com>
2025-10-26 13:04:43 +0200

    fixing the query parser

--- server.js
+++ server.js
@@ -32,10 +32,11 @@ const reviews = require("./routes/reviews");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.set("query parser", "extended");
+// app.set("query parser", "extended");
 
 // Body parser
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // ✅ Add this for query parsing
 
 // Cookie parser
 app.use(cookieParser());
